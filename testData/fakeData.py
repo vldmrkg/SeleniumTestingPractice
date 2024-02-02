34d36f25aca680e533fe5155fa1c11e924a5324b
Vladimir <145365445+vldmrkg@users.noreply.github.com>
2024-02-02 20:50:20 +0100

    Update fakeData.py

--- testData/fakeData.py
+++ testData/fakeData.py
@@ -30,6 +30,23 @@ class CheckOutPageData:
         }
     ]
 
+class SignInPageData:
+    # Lista sa test podacima za SignInPage
+    test_signInPage_data = [
+        {
+            "correct_email": "vldmr@gmail.comm",
+            "correct_password": "Lalalala123123",
+            "incorrect_email": "vldmra@gmail.comm",
+            "incorrect_password": "Lalalala123123sa"
+        }
+    ]
 
+    @classmethod
+    def get_correct_signin_data(cls):
+        return cls.test_signInPage_data[0]["correct_email"], cls.test_signInPage_data[0]["correct_password"]
+
+    @classmethod
+    def get_incorrect_signin_data(cls):
+        return cls.test_signInPage_data[0]["incorrect_email"], cls.test_signInPage_data[0]["incorrect_password"]
 
 
