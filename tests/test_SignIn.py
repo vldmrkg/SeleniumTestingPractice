3d32f9d3f43f73c39be3c8a56e5284ef33f90c5f
Vladimir <145365445+vldmrkg@users.noreply.github.com>
2024-02-03 19:08:36 +0100

    Update test_SignIn.py

--- tests/test_SignIn.py
+++ tests/test_SignIn.py
@@ -1,42 +1,24 @@
 from locators.CustomerLoginPage import CustomerLogin
-from locators.MyAccountPage import MyAccount
+from testData.fakeData import SignInPageData
 from utilities.BaseClass import BaseClass
 from locators.HomePage import HomePage
 
 
 class TestSignIn(BaseClass):
 
-    def perform_sign_in(self, email, password, expected_welcome_message=None):
-        log = self.getLogger()
-        log.info(f"Testing sign-in with {'correct' if expected_welcome_message else 'incorrect'} credentials begins.")
-
-        homePage = HomePage(self.driver)
-        homePage.sign_in()
-        self.driver.implicitly_wait(15)
-
-        logInPage = CustomerLogin(self.driver)
-        assert logInPage.page_title() == "Customer Login"
-
-        logInPage.enter_email(email)
-        logInPage.enter_password(password)
-        logInPage.sign_in_button()
-
-        if expected_welcome_message:
-            assert homePage.welcome_user_message() == expected_welcome_message
-            logOut = MyAccount(self.driver)
-            logOut.click_sign_out()
-            log.info("Testing sign-in with correct credentials completed.")
-        else:
-            assert "The account sign-in was incorrect" in logInPage.alert_message()
-            log.info("Testing sign-in with incorrect credentials completed.")
-
     def test_sign_in_correct_data(self):
+        home_page = HomePage(self.driver)
+        log_in_page = CustomerLogin(self.driver)
+
         correct_email, correct_password = SignInPageData.get_correct_signin_data()
-        self.perform_sign_in(correct_email, correct_password, "Welcome, Vladimir Timotijevic!")
+        log_in_page.perform_sign_in(correct_email, correct_password, home_page, "Welcome, Vladimir Timotijevic!")
 
     def test_sign_in_incorrect_data(self):
+        home_page = HomePage(self.driver)
+        log_in_page = CustomerLogin(self.driver)
+
         incorrect_email, incorrect_password = SignInPageData.get_incorrect_signin_data()
-        self.perform_sign_in(incorrect_email, incorrect_password)
+        log_in_page.perform_sign_in(incorrect_email, incorrect_password, home_page)
 
 
 
