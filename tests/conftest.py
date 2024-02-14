6fa2a6d80b8e9eaee03b5fc7d9b042e453f51895
Vladimir Timotijevic <v.timotijevic@outlook.com>
2024-02-14 16:22:46 +0100

    Refactored test setup to utilize WebDriver Manager for dynamic driver installation based on specified browser, enhancing maintainability and eliminating manual driver management.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,57 +1,46 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.opera import OperaDriverManager
 import os
 
 
-URL = ("https://magento.softwaretestingboard.com")
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--browser_name", action="store", default="chrome"
-    )
+URL = "https://magento.softwaretestingboard.com"
 
 
 @pytest.fixture(scope="class")
 def setup(request):
-
     browser_name = request.config.getoption("browser_name")
-    screenshot_dir = r"C:\Screenshots"
 
     if browser_name == "chrome":
-        driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\\chromedriver.exe")
-
+        driver = webdriver.Chrome(ChromeDriverManager().install())
     elif browser_name == "opera":
-        driver = webdriver.Opera(executable_path="C:\\Program Files (x86)\\operadriver.exe")
-
+        driver = webdriver.Opera(OperaDriverManager().install())
     elif browser_name == "edge":
-        driver = webdriver.Edge(executable_path="C:\\Program Files (x86)\\msedgedriver.exe")
-
+        driver = webdriver.Edge(EdgeChromiumDriverManager().install())
     elif browser_name == "firefox":
-        driver = webdriver.Firefox(executable_path="C:\\Program Files (x86)\\geckodriver.exe")
-
+        driver = webdriver.Firefox(GeckoDriverManager().install())
     else:
         raise ValueError(f"Unsupported browser: {browser_name}")
 
     driver.get(URL)
     driver.maximize_window()
 
-    #provera da li je pocetna stranica ispravno ucitana
-
+    # Provera da li je pocetna stranica ispravno ucitana
     expected_title = "Home Page"
     actual_title = driver.title
-
     assert actual_title == expected_title
 
     request.cls.driver = driver
 
     yield
 
-    if request.session.testsfailed:
-        test_name = request.node.name
-        screenshot_path = os.path.join(screenshot_dir, f"{test_name}_failure.png")
-        driver.save_screenshot(screenshot_path)
-        print(f"\nScreenshot saved: {screenshot_path}")
+    driver.quit()
 
-    driver.close()
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome", help="Specify browser name: chrome, opera, edge, firefox"
+    )
